1545cc294e7f2afedcf65d1ffa68307cd29a5772
Dries007 <admin@dries007.net>
2018-04-20 12:12:07 +0200

    made pi more RAM efficient

--- pi/main.c
+++ pi/main.c
@@ -35,29 +35,34 @@ int main(int argc, char *argv[])
     printf("Pi estimation: %lf\n", pi);
 }
 
+cl_mem generateRnd(size_t n)
+{
+    cl_int error;
+    cl_float * host = calloc(n, sizeof(cl_float));
+    if (host == NULL) abort();
+    for (int i = 0; i < n; i++)
+    {
+        host[i] = ((float)rand()/(float)RAND_MAX);
+    }
+    cl_mem dev_inpx = clCreateBuffer(g_context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * n, host, &error);
+    ocl_err(clFinish(g_command_queue));
+    ocl_err(error);
+    free(host);
+    return dev_inpx;
+}
+
 double estimate_pi(const cl_uint n)
 {
     const cl_uint group_size = 64;
     const cl_uint groups = n / group_size;
 
-    cl_float * inpx = calloc(n, sizeof(cl_float));
-    cl_float * inpy = calloc(n, sizeof(cl_float));
-    cl_uint * outp = calloc(groups, sizeof(cl_uint));
-
     srand((unsigned int)time(NULL));
-    for (int i = 0; i < n; i++)
-    {
-        inpx[i] = ((float)rand()/(float)RAND_MAX);
-        inpy[i] = ((float)rand()/(float)RAND_MAX);
-    }
+
+    cl_mem dev_inpx = generateRnd(groups * group_size);
+    cl_mem dev_inpy = generateRnd(groups * group_size);
 
     cl_int error;
 
-    // Input buffer on GPU
-    cl_mem dev_inpx = clCreateBuffer(g_context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * n, inpx, &error);
-    ocl_err(error);
-    cl_mem dev_inpy = clCreateBuffer(g_context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * n, inpy, &error);
-    ocl_err(error);
     // Result buffer on GPU
     cl_mem dev_result = clCreateBuffer(g_context, CL_MEM_WRITE_ONLY, sizeof(cl_uint) * groups, NULL, &error);
     ocl_err(error);
@@ -83,6 +88,7 @@ double estimate_pi(const cl_uint n)
 
     // Read result
     time_measure_start("data_transfer");
+    cl_uint * outp = calloc(groups, sizeof(cl_uint));
     ocl_err(clEnqueueReadBuffer(g_command_queue, dev_result, CL_TRUE, 0, sizeof(cl_uint) * groups, outp, 0, NULL, NULL));
     time_measure_stop_and_print("data_transfer");
 
@@ -93,7 +99,6 @@ double estimate_pi(const cl_uint n)
     {
         trues += outp[i];
         _n += group_size;
-//        printf("%d (%d/%d) %lf\n", outp[i], trues, _n, 2.0 * ((double)trues / (double)_n));
     }
     time_measure_stop_and_print("count");
     return 4.0 * ((double)trues / (double)_n);
